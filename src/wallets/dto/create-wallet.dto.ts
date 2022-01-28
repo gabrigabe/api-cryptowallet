2b061402f57a91c8b12a37f36b14415a6ad8b938
Gabriel Bezerra Rodrigues <gabriel.bezerra@compasso.com.br>
2022-01-28 16:35:20 -0300

    feat(validators): adds trim validation to createWallets

--- src/wallets/dto/create-wallet.dto.ts
+++ src/wallets/dto/create-wallet.dto.ts
@@ -1,4 +1,5 @@
-import { IsString, Matches, MinLength, Validate } from 'class-validator';
+import { Transform, TransformFnParams } from 'class-transformer';
+import { IsNotEmpty, IsString, Matches, MinLength, Validate } from 'class-validator';
 import { AgeChecker } from '../utils/validations/checkAge';
 import { CpfChecker } from '../utils/validations/checkCPF';
 import { DateChecker } from '../utils/validations/checkDate';
@@ -6,14 +7,20 @@ import { DateChecker } from '../utils/validations/checkDate';
 export class CreateWalletDto {
     @IsString()
     @MinLength(7)
+    @IsNotEmpty()
+    @Transform(({ value }: TransformFnParams) => value.trim())
     readonly name!: string;
 
     @IsString()
+    @IsNotEmpty()
+    @Transform(({ value }: TransformFnParams) => value.trim())
     @Matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/)
+    @IsNotEmpty()
     @Validate(CpfChecker)
     readonly cpf!: string;
 
     @IsString()
+    @IsNotEmpty()
     @Validate(DateChecker)
     @Validate(AgeChecker)
     readonly birthdate!: string;
